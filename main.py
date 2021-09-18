29b77fa1a65ff498bee688e1266375579c11f206
Aayaan <66046533+AayaanN@users.noreply.github.com>
2021-09-18 18:34:14 -0400

    Formatting

--- main.py
+++ main.py
@@ -33,23 +33,27 @@ async def on_message(message):
     if message.content == '!hello':
         response = "Hi, I am Apollo! Type !screen to proceed with the screening."
         await message.channel.send(response)
+
+
     elif message.content == '!screen':
-        response = "Let the screening begin... \nDo any of the following apply to you?\nI am fully vaccinated against COVID-19 (it has been 14 days or more since your final dose of either a two-dose or a one-dose vaccine series)\n To reply yes, type y, and to reply no, type n "
+        response = "**Let the screening begin... \nDo any of the following apply to you?**\n\n*a. I am fully vaccinated " \
+                   "against " \
+                   "COVID-19 (it has been 14 days or more since your final dose of either a two-dose or a one-dose " \
+                   "vaccine series)*\n\n *b. I have tested positive for COVID-19 in the last 90 days (and since been " \
+                   "cleared)*\n\n** To reply yes, type y, and to reply no, type n **"
         await message.channel.send(response)
-        
+
         def check(msg):
             return msg.author == message.author and msg.channel == message.channel and msg.content.lower() in ["y", "n"]
-            
 
-        msg = await client.wait_for("message", check = check) 
+        msg = await client.wait_for("message", check=check)
         if msg.content.lower() == "y":
             await message.channel.send("You said yes!")
         else:
             await message.channel.send("You said no!")
 
-    #if message.content.find("!hello") != -1:
-        #await message.channel.send("Hi, I am Apollo! Type !screen to proceed with the screening.")
-    
+    # if message.content.find("!hello") != -1:
+    # await message.channel.send("Hi, I am Apollo! Type !screen to proceed with the screening.")
 
 
 client.run("ODg4Nzg3MjE4NzAzOTk5MDE2.YUXxmQ.qR-61OtwZuJj2DWEqM3oKGU4Vxk")
