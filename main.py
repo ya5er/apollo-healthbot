a82e8bad67537f28c107992d603f2394abefafe0
Aayaan <66046533+AayaanN@users.noreply.github.com>
2021-09-18 19:33:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def on_message(message):
         response = "Hi, I am Apollo! Type !screen to proceed with the screening."
         await message.channel.send(response)
 
-# Level 1:
+    # Level 1:
     elif message.content == '!screen':
         response = "**Let the screening begin... \nDo any of the following apply to you? **\n\n*a. I am fully vaccinated " \
                    "against " \
@@ -47,30 +47,41 @@ async def on_message(message):
             return msg.author == message.author and msg.channel == message.channel and msg.content.lower() in ["y", "n"]
 
         msg = await client.wait_for("message", check=check)
-# Level 2, YY
+        # Level 2, YY
         if msg.content.lower() == "y":
-            await message.channel.send("In the last 14 days, have you travelled outside of Canada and been told to "
-                                       "quarantine (per the federal quarantine requirements)?")
+            await message.channel.send("**In the last 14 days, have you travelled outside of Canada and been told to "
+                                       "quarantine (per the federal quarantine requirements)?**")
 
             msg = await client.wait_for("message", check=check)
 
             if msg.content.lower() == "y":
                 await message.channel.send("**DO NOT ATTEND THIS EVENT**")
-# Level 2, YN
+            # Level 2, YN
             else:
-                await message.channel.send("Has a doctor, health care provider, or public health unit told you that "
-                                           "you should currently be isolating (staying at home)?")
+                await message.channel.send("**Has a doctor, health care provider, or public health unit told you that "
+                                           "you should currently be isolating (staying at home)?**")
 
-            msg = await client.wait_for("message", check=check)
-#Level 3, YNY
-            if msg.content.lower() == "y":
-                await message.channel.send("**DO NOT ATTEND THIS EVENT**")
+                msg = await client.wait_for("message", check=check)
+                # Level 3, YNY
+                if msg.content.lower() == "y":
+                    await message.channel.send("**DO NOT ATTEND THIS EVENT**")
 
-            else: await message.channel.send("Are you currently experiencing any of these symptoms?\n\n")
+                else:
+                    await message.channel.send(
+                        "**Are you currently experiencing any of these symptoms?**\n\n**Fever and/or "
+                        "chills**\nTemperature of 37.8 degrees Celsius/100 degrees Fahrenheit or "
+                        "higher\n\n**Cough or barking cough**\nContinuous, more than usual, making a "
+                        "whistling noise when breathing (not related to asthma, post-infectious "
+                        "reactive airways, or other known causes or conditions you already "
+                        "have)\n\n**Shortness of breath**\nOut of breath, unable to breathe deeply "
+                        "(not related to asthma or other known causes or conditions you already "
+                        "have)\n\n**Decrease or loss of taste or smell**\nNot related to seasonal "
+                        "allergies, neurological disorders, or other known causes or conditions "
+                        "you already have\n\n**Nausea, vomiting, and/or diarrhea**\nNot related to "
+                        "irritable bowel syndrome, anxiety, menstrual cramps, or other known "
+                        "causes or conditions you already have")
 
-
-
-# Level 2, N, YASER START HERE
+        # Level 2, N, YASER START HERE
         else:
             await message.channel.send("You said no!")
 
