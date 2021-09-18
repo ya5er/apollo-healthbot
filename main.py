d37798fed22d8e11997d6a13079cc817b7f3349f
ya5er <75911486+ya5er@users.noreply.github.com>
2021-09-18 16:29:16 -0400

    Screening stuff

--- main.py
+++ main.py
@@ -12,15 +12,34 @@
 # client.run(TOKEN)
 
 import discord
+import random
 
 client = discord.Client()
 
 
 @client.event
 async def on_message(message):
-    if message.content.find("!hello") != -1:
-        # If the user says !hello we will send back hi
-        await message.channel.send("Hi, I am Apollo!")
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 100 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+    if message.content == '!hello':
+        response = "Hi, I am Apollo! Type !screen to proceed with the screening."
+        await message.channel.send(response)
+    elif message.content == '!screen':
+        response = "Let the screening begin..."
+        await message.channel.send(response)
+
+    #if message.content.find("!hello") != -1:
+        #await message.channel.send("Hi, I am Apollo! Type !screen to proceed with the screening.")
+    
 
 
 client.run("ODg4Nzg3MjE4NzAzOTk5MDE2.YUXxmQ.qR-61OtwZuJj2DWEqM3oKGU4Vxk")
