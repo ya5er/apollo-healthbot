727a79e4f87e299ceb12a52737b0f40248dc07b8
ya5er <75911486+ya5er@users.noreply.github.com>
2021-09-18 13:13:43 -0400

    working

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-import discord
-import random
+# import discord
+# import random
+#
+# TOKEN = 'ODg4Nzg3MjE4NzAzOTk5MDE2.YUXxmQ.qR-61OtwZuJj2DWEqM3oKGU4Vxk'
+#
+# client = discord.Client()
+#
+# @client.event
+# async def on_ready():
+#     print('We have logged in as {0.user}'.format(client))
+#
+# client.run(TOKEN)
 
-TOKEN = 'ODg4Nzg3MjE4NzAzOTk5MDE2.YUXxmQ.qR-61OtwZuJj2DWEqM3oKGU4Vxk'
+import discord
 
 client = discord.Client()
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
 
-client.run(TOKEN)
+@client.event
+async def on_message(message):
+    if message.content.find("!hello") != -1:
+        # If the user says !hello we will send back hi
+        await message.channel.send("Hi, I am Apollo!")
+
+
+client.run("ODg4Nzg3MjE4NzAzOTk5MDE2.YUXxmQ.qR-61OtwZuJj2DWEqM3oKGU4Vxk")
+# client.run(token)
+
+print("I'm Online")
